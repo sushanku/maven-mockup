fe24e25d827b7e8f42f8b9143ced8afc99468942
Sushan Kunwar <sushankunwar01@gmail.com>
2020-02-03 00:47:51 +0545

    Final Setup

--- jenkins/scripts/test.sh
+++ jenkins/scripts/test.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env sh
 
 echo 'Deploy before testing'
-pwd
 cp mockup_db.sql /tmp
 ansible-playbook -i ansible/localhost ansible/mysqlsetup.yml -t "create_db, add_user, import_db"
 set -x
@@ -10,6 +9,6 @@ set +x
 cd testcase/
 mvn clean install
 mvn test "-Dtest=Test.Runner"
-pwd
+cd ..
 ansible-playbook -i ansible/localhost ansible/mysqlsetup.yml -t "delete_user, remove_db"
 rm -rf /tmp/mockup_db.sql
