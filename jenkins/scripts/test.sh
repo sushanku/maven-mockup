9b61dea45307eeb0f57fbcafb33fbf10f5b04042
Sushan Kunwar <sushankunwar01@gmail.com>
2020-02-03 00:41:55 +0545

    db location configured

--- jenkins/scripts/test.sh
+++ jenkins/scripts/test.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env sh
 
 echo 'Deploy before testing'
+pwd
+cp mockup_db.sql /tmp
 ansible-playbook -i ansible/localhost ansible/mysqlsetup.yml -t "create_db, add_user, import_db"
 set -x
 java -jar target/test-1.0-SNAPSHOT-jar-with-dependencies.jar &
@@ -8,4 +10,6 @@ set +x
 cd testcase/
 mvn clean install
 mvn test "-Dtest=Test.Runner"
+pwd
 ansible-playbook -i ansible/localhost ansible/mysqlsetup.yml -t "delete_user, remove_db"
+rm -rf /tmp/mockup_db.sql
