c3fff9a201dd2102e2635f809787d863b1936932
Sushan Kunwar <sushankunwar01@gmail.com>
2020-02-03 00:57:39 +0545

    Deployment Setup also s:xettled

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 
 echo 'Deploy Finally'
+cp mockup_db.sql /tmp
 ansible-playbook -i ansible/localhost ansible/mysqlsetup.yml -t "create_db, add_user, import_db"
 rm -rf /home/jenkins/deploy/maven-mockup/*
 cp target/test-1.0-SNAPSHOT-jar-with-dependencies.jar /home/jenkins/deploy/maven-mockup
@@ -8,3 +9,4 @@ cp -r etc /home/jenkins/deploy/maven-mockup/
 set -x
 java -jar /home/jenkins/deploy/maven-mockup/test-1.0-SNAPSHOT-jar-with-dependencies.jar &
 set +x
+rm mockup_db.sql /tmp
