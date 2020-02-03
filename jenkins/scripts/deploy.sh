b4fa803db4a85f2d1081c5c1f6efb2e6b1421161
Sushan Kunwar <sushankunwar01@gmail.com>
2020-02-03 14:43:27 +0545

    Ansible and scripts is modified to deploy

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -2,7 +2,7 @@
 
 echo 'Deploy Finally'
 cp mockup_db.sql /tmp
-ansible-playbook -i ansible/localhost ansible/mysqlsetup.yml -t "create_db, add_user, import_db"
+ansible-playbook -i localhost --extra-vars "db_name=osticket host=localhost username=osticket password=ticket123" -t "create_db, add_user, import_db" ansible/mysqlsetup.yml
 rm -rf /tmp/mockup_db.sql
 rm -rf /home/jenkins/deploy/maven-mockup/*
 cp target/test-1.0-SNAPSHOT-jar-with-dependencies.jar /home/jenkins/deploy/maven-mockup
