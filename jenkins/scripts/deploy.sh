ea62cac43a55dadffaf643cd642b568f4af21adf
Sushan Kunwar <sushankunwar01@gmail.com>
2020-02-03 14:53:49 +0545

    Ansible and scripts is modified to match db properties

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -2,7 +2,7 @@
 
 echo 'Deploy Finally'
 cp mockup_db.sql /tmp
-ansible-playbook -i localhost --extra-vars "db_name=osticket host=localhost username=osticket password=ticket123" -t "create_db, add_user, import_db" ansible/mysqlsetup.yml
+ansible-playbook -i localhost --extra-vars "db_name=mockup_db host=localhost username=mockup password=mockup123" -t "create_db, add_user, import_db" ansible/mysqlsetup.yml
 rm -rf /tmp/mockup_db.sql
 rm -rf /home/jenkins/deploy/maven-mockup/*
 cp target/test-1.0-SNAPSHOT-jar-with-dependencies.jar /home/jenkins/deploy/maven-mockup
