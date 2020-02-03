f49e8367b66bb0cb6eef0f11d72027fe7856531d
Sushan Kunwar <sushankunwar01@gmail.com>
2020-02-03 14:35:21 +0545

    Ansible and scripts is modified

--- jenkins/scripts/test.sh
+++ jenkins/scripts/test.sh
@@ -2,7 +2,7 @@
 
 echo 'Deploy before testing'
 cp mockup_db.sql /tmp
-ansible-playbook -i ansible/localhost ansible/mysqlsetup.yml -t "create_db, add_user, import_db"
+ansible-playbook -i localhost --extra-vars "db_name=osticket host=localhost username=osticket password=ticket123" -t "create_db, add_user, import_db" ansible/mysqlsetup.yml
 set -x
 java -jar target/test-1.0-SNAPSHOT-jar-with-dependencies.jar &
 set +x
@@ -10,5 +10,5 @@ cd testcase/
 mvn clean install
 mvn test "-Dtest=Test.Runner"
 cd ..
-ansible-playbook -i ansible/localhost ansible/mysqlsetup.yml -t "delete_user, remove_db"
+ansible-playbook -i localhost --extra-vars "db_name=osticket username=osticket" -t "delete_user, remove_db" ansible/mysqlsetup.yml
 rm -rf /tmp/mockup_db.sql
