d4c6f8a187b774640f8106c8e8c2a27e13ff21be
Sushan Kunwar <sushankunwar01@gmail.com>
2020-02-11 16:35:10 +0545

    script and jenkinfile modified to be deployed locally by using ansible which further users module to create user and deploy

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -4,12 +4,11 @@ echo 'Deploy Finally'
 cp mockup_db.sql /tmp
 #mkdir -p /home/jenkins/deploy/maven-mockup
 ansible-playbook -i ansible/localhost --extra-vars "db_name=mockup_db host=localhost username=mockup password=mockup123" -t "create_db, add_user, import_db" ansible/mysqlsetup.yml
-ansible-playbook -i ansible/localhost  --extra-vars "deploy_user=mockup" -t "add_new_user, copy_pub_key, generate_ssh_keys" usersetup.yml
+ansible-playbook -i ansible/localhost  --extra-vars "deploy_user=mockup" -t "add_new_user, copy_pub_key, generate_ssh_keys, create_dir" usersetup.yml
 rm -rf /tmp/mockup_db.sql
-rm -rf /home/jenkins/deploy/maven-mockup/*
-rsync -avz --stats --progress target/test-1.0-SNAPSHOT-jar-with-dependencies.jar mockup@localhost:/home/mockup/
-rsync -avz --stats --progress etc mockup@localhost:/home/mockup/
+rsync -avz --stats --progress target/test-1.0-SNAPSHOT-jar-with-dependencies.jar mockup@localhost:/home/mockup/deploy/
+rsync -avz --stats --progress etc mockup@localhost:/home/mockup/deploy/
 ssh mockup@localhost
 set -x
-java -jar /home/mockup/test-1.0-SNAPSHOT-jar-with-dependencies.jar &
+java -jar /home/mockup/deploy/test-1.0-SNAPSHOT-jar-with-dependencies.jar &
 set +x
