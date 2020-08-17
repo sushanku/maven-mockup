a95de8d45dcc5bead1a3e3ba7a090b327936d52b
Sushan Kunwar <sushankunwar01@gmail.com>
2020-08-17 16:02:19 +0545

    Update test.sh

--- jenkins/scripts/test.sh
+++ jenkins/scripts/test.sh
@@ -4,7 +4,7 @@ echo 'Deploy before testing'
 cp mockup_db.sql /tmp
 ansible-playbook -i ansible/localhost --extra-vars "db_name=mockup_db host=localhost username=mockup password=mockup123" -t "create_db, add_user, import_db" ansible/mysqlsetup.yml
 set -x
-java -jar /home/jenkins/deploy/maven-mockup/test-1.0-SNAPSHOT-jar-with-dependencies.jar &
+java -jar target/test-1.0-SNAPSHOT-jar-with-dependencies.jar &
 set +x
 cd testcase/
 mvn clean install
