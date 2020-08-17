889dff3739932f7e0173de1f411dfa12bb87a5ea
Sushan Kunwar <sushankunwar01@gmail.com>
2020-08-17 16:36:44 +0545

    Update test.sh

--- jenkins/scripts/test.sh
+++ jenkins/scripts/test.sh
@@ -7,7 +7,6 @@ set -x
 java -jar target/test-1.0-SNAPSHOT-jar-with-dependencies.jar &
 set +x
 cd testcase/
-mvn clean install
 mvn test "-Dtest=Test.Runner"
 cd ..
 ansible-playbook -i ansible/localhost --extra-vars "db_name=mockup_db username=mockup" -t "delete_user, remove_db" ansible/mysqlsetup.yml
