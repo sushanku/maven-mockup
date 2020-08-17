ac10b0e9ccf2ca9f73bea91b402536ac41d6cdac
Sushan Kunwar <sushankunwar01@gmail.com>
2020-08-17 23:44:48 +0545

    Update deploy.sh

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -6,7 +6,7 @@ cp mockup_db.sql /tmp
 ansible-playbook -i ansible/localhost --extra-vars "db_name=mockup_db host=localhost username=mockup password=mockup123" -t "create_db, add_user, import_db" ansible/mysqlsetup.yml
 ansible-playbook -i ansible/localhost --extra-vars "deploy_user=mockup" -t "add_new_user,copy_pub_key,generate_ssh_keys,create_dir" ansible/usersetup.yml
 rm -rf /tmp/mockup_db.sql
-rsync -avz --stats --progress target/test-1.0-SNAPSHOT-jar-with-dependencies.jar mockup@localhost:/home/mockup/deploy/
+rsync -avz --stats --progress /tmp/test-1.0-SNAPSHOT-jar-with-dependencies.jar mockup@localhost:/home/mockup/deploy/
 rsync -avz --stats --progress etc mockup@localhost:/home/mockup/deploy/
 ssh mockup@localhost
 set -x
