a65d08ec5469feb44a294d09b24e0b5f3aa075bc
sushanku <sushankunwar01@gmail.com>
2020-08-20 19:33:41 +0545

    mysql file copied to remote production server is added in ansible playbook

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -2,7 +2,7 @@
 
 echo 'Deploy Finally'
 #ansible playbook to create and import database with user permission
-ansible-playbook -i ansible/hosts --limit production -t "add_new_user,copy_pub_key,generate_ssh_keys,create_dir,copy_deploy_file" ansible/usersetup.yml
+ansible-playbook -i ansible/hosts --limit production -t "add_new_user,copy_pub_key,generate_ssh_keys,create_dir,copy_deploy_file,copy_DB" ansible/usersetup.yml
 ansible-playbook -i ansible/hosts --limit production --extra-vars "db_name=mockup_db host=localhost username=mockup password=mockup123" -t "create_db,add_user,import_db" ansible/mysqlsetup.yml
 #ansible playbook to create user and transfer the neccessary deployment files and deploy it.
 #ansible-playbook -i ansible/hosts --limit production -t "deploy_jar" ansible/usersetup.yml
