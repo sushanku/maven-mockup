b878203c67877a011fb66bbd15fea8287a177a1e
Sushan Kunwar <sushankunwar01@gmail.com>
2020-08-20 18:29:57 +0545

    updated to deploy by ssh instead of ansible

--- jenkins/scripts/test.sh
+++ jenkins/scripts/test.sh
@@ -6,7 +6,12 @@ cp target/test-1.0-SNAPSHOT-jar-with-dependencies.jar /tmp
 ansible-playbook -i ansible/hosts --limit localhost -t "add_new_user,copy_pub_key,generate_ssh_keys,create_dir,copy_deploy_file" ansible/usersetup.yml
 ansible-playbook -i ansible/hosts --limit localhost --extra-vars "db_name=mockup_db host=localhost username=mockup password=mockup123" -t "create_db,add_user,import_db" ansible/mysqlsetup.yml
 #ansible playbook to create user and transfer the neccessary deployment files and deploy it.
-ansible-playbook -i ansible/hosts --limit localhost -t "deploy_jar" ansible/usersetup.yml
+#ansible-playbook -i ansible/hosts --limit localhost -t "deploy_jar" ansible/usersetup.yml
+ssh mockup@localhost
+cd /home/mockup/deploy
+set -x
+java -jar target/test-1.0-SNAPSHOT-jar-with-dependencies.jar &
+set +x
 
 ##FUNCTIONAL Testing
 cd testcase/
